8d2b998745d8c981f05acc9f3b29b197d8b8c329
ablaamim <aku-no-hana@outlook.fr>
2022-03-25 13:46:44 +0100

    data initialization done

--- push_swap_in_a_nutshell.c
+++ push_swap_in_a_nutshell.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 11:22:05 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/03/25 13:32:28 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/03/25 13:46:26 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,23 @@ typedef struct s_data
 	void	(*ft[2])(struct s_data *, int, int);
 }	t_data;
 
-enum e_order{
+enum e_order
+{
 	ASCENDING,
 	DESCENDING
 };
+
+enum e_operation
+{
+	ROTATE,
+	REVERSE_ROTATE,
+	SWAP,
+	PUSH
+};
+
+# define A		0
+# define B		1
+# define R		2
 
 void	ft_clear_list(t_list **list)
 {
@@ -189,11 +202,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	if (!dst || !src)
-		return (0);
-	i = 0;
-	if (dstsize > 0)
+		return (0x0);
+	i = 0x0;
+	if (dstsize > 0x0)
 	{
-		while (src[i] && i + 1 < dstsize)
+		while (src[i] && i + 0x1 < dstsize)
 		{
 			dst[i] = src[i];
 			i++;
@@ -208,21 +221,21 @@ char	**ft_args_copy(char **args, int size)
 	char	**copied;
 	int		i;
 
-	copied = (char **)malloc(sizeof(char *) * (size + 1));
+	copied = (char **)malloc(sizeof(char *) * (size + 0x1));
 	if (!copied)
 		return (NULL);
-	i = 0;
+	i = 0x0;
 	while (i < size)
 	{
-		copied[i] = (char *)malloc(sizeof(char) * (ft_strlen(args[i]) + 1));
+		copied[i] = (char *)malloc(sizeof(char) * (ft_strlen(args[i]) + 0x1));
 		if (!copied[i])
 		{
-			while (--i >= 0)
+			while (--i >= 0x0)
 				free(copied[i]);
 			free(copied);
 			return (NULL);
 		}
-		ft_strlcpy(copied[i], args[i], ft_strlen(args[i]) + 1);
+		ft_strlcpy(copied[i], args[i], ft_strlen(args[i]) + 0x1);
 		i++;
 	}
 	copied[i] = NULL;
@@ -275,7 +288,7 @@ void	ft_stack_range(t_stack **stack)
 	int		max;
 
 	list = (*stack)->list;
-	(*stack)->size = 0;
+	(*stack)->size = 0x0;
 	if (list)
 	{
 		min = (*stack)->list->number;
@@ -292,7 +305,7 @@ void	ft_stack_range(t_stack **stack)
 		(*stack)->range = (size_t)(max - min);
 	}
 	else
-		(*stack)->range = 0;
+		(*stack)->range = 0x0;
 }
 
 int	ft_isdigit(int c)
@@ -306,9 +319,9 @@ int	ft_atoi_mod(t_data *data, int position)
 	int		sign;
 	long	total;
 
-	i = 0;
-	sign = 1;
-	total = 0;
+	i = 0x0;
+	sign = 0x1;
+	total = 0x0;
 	if (data->args[position][i] == '+' || data->args[position][i] == '-')
 		if (data->args[position][i++] == '-')
 			sign *= (-1);
@@ -335,7 +348,7 @@ void	ft_get_stack_list(t_data *data)
 	int		i;
 
 	data->a->list = NULL;
-	i = 0;
+	i = 0x0;
 	while (data->args[i])
 	{
 		new = ft_list_new(ft_atoi_mod(data, i));
@@ -364,6 +377,119 @@ void	ft_init_stacks(t_data *data)
 	data->b->order = DESCENDING;
 }
 
+int	ft_check_args(t_data *data)
+{
+	t_list	*tmp;
+	t_list	*iterator;
+
+	tmp = data->a->list;
+	while (tmp)
+	{
+		iterator = tmp->next;
+		while (iterator)
+		{
+			if (tmp->number == iterator->number)
+				return (0x0);
+			iterator = iterator->next;
+		}
+		tmp = tmp->next;
+	}
+	return (0x1);
+}
+
+void	ft_stack_rotate(t_stack **stack)
+{
+	t_list	*moved;
+	t_list	**list;
+
+	moved = (*stack)->list;
+	(*stack)->list = (*stack)->list->next;
+	moved->next = NULL;
+	list = &(*stack)->list;
+	ft_list_add_back(list, moved);
+}
+
+void	ft_print_movement(int operation, int stack_id)
+{
+	if (operation == SWAP)
+		write(1, "s", 1);
+	else if (operation == PUSH)
+		write(1, "p", 1);
+	else if (operation == ROTATE)
+		write(1, "r", 1);
+	else if (operation == REVERSE_ROTATE)
+		write(1, "rr", 2);
+	if (stack_id == A)
+		write(1, "a\n", 2);
+	else if (stack_id == B)
+		write(1, "b\n", 2);
+	else if (stack_id == R && operation != SWAP)
+		write(1, "r\n", 2);
+	else
+		write(1, "s\n", 2);
+}
+
+void	ft_rotate(t_data *data, int id, int is_print)
+{
+	if (id == A)
+		ft_stack_rotate(&data->a);
+	else if (id == B)
+		ft_stack_rotate(&data->b);
+	else if (id == R)
+	{
+		ft_stack_rotate(&data->a);
+		ft_stack_rotate(&data->b);
+	}
+	if (is_print)
+		ft_print_movement(ROTATE, id);
+}
+
+size_t	ft_list_len(t_list *list)
+{
+	size_t	size;
+
+	size = 0;
+	while (list)
+	{
+		size++;
+		list = list->next;
+	}
+	return (size);
+}
+
+void	ft_stack_reverse_rotate(t_stack **stack)
+{
+	t_list	*moved;
+	t_list	*temp;
+	size_t	list_size;
+	size_t	i;
+
+	list_size = ft_list_len((*stack)->list);
+	moved = ft_list_last((*stack)->list);
+	moved->next = (*stack)->list;
+	(*stack)->list = moved;
+	temp = (*stack)->list;
+	i = 0;
+	while (++i < list_size)
+		temp = temp->next;
+	temp->next = NULL;
+}
+
+void	ft_reverse_rotate(t_data *data, int id, int is_print)
+{
+	if (id == A)
+		ft_stack_reverse_rotate(&data->a);
+	else if (id == B)
+		ft_stack_reverse_rotate(&data->b);
+	else if (id == R)
+	{
+		ft_stack_reverse_rotate(&data->a);
+		ft_stack_reverse_rotate(&data->b);
+	}
+	if (is_print)
+		ft_print_movement(REVERSE_ROTATE, id);
+}
+
 t_data	*ft_data_initializer(int argc, char **argv)
 {
 	t_data	*data;
@@ -376,6 +502,14 @@ t_data	*ft_data_initializer(int argc, char **argv)
 	else
 		data->args = ft_args_copy(&argv[1], argc - 1);
 	ft_init_stacks(data);
+	if (!ft_check_args(data))
+		ft_end_program(data, 2);
+	data->pos = (int *)malloc(sizeof(int) * 2);
+	data->tmp = (int *)malloc(sizeof(int) * 2);
+	data->ft[ROTATE] = ft_rotate;
+	data->ft[REVERSE_ROTATE] = ft_reverse_rotate;
+	if (!data->pos || !data->tmp)
+		ft_end_program(data, 4);
 	return (data);
 }
 
